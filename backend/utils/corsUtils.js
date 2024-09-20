590939d7fe6de898fd829005c7fab9d735b77035
Florent Grondin <fgrondin@qualisocial.com>
2024-09-20 16:15:32 +0200

    add corsUtils

---
+++ backend/utils/corsUtils.js
@@ -0,0 +1,24 @@
+const cors = require('cors');
+
+const allowedOrigins = [
+    'http://localhost',
+    //'http://'
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+const corsMiddleware = cors(corsOptions);
+
+module.exports = corsMiddleware;
