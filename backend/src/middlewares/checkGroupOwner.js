8dc7c29f5fdcbdcb8c23d46f27f544e99c4eec22
Florent Grondin <fgrondin@qualisocial.com>
2024-10-16 16:47:04 +0200

    add membership function and validators and entity

---
+++ backend/src/middlewares/checkGroupOwner.js
@@ -0,0 +1,19 @@
+exports.checkGroupOwner = async (req, res, next) => {
+    try {
+        const { groupId } = req.params;
+        const userId = req.user.id; 
+
+        const group = await Group.findById(groupId);
+        if (!group) {
+            return res.status(404).json({ message: 'Groupe non trouvé' });
+        }
+
+        if (group.ownerId.toString() !== userId) {
+            return res.status(403).json({ message: 'Accès interdit: vous n\'êtes pas le créateur du groupe' });
+        }
+
+        next();
+    } catch (error) {
+        res.status(500).json({ message: 'Erreur serveur', error });
+    }
+};
