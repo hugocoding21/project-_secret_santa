a9970939e9546c580484cbefb8a852389ca5e466
Florent Grondin <fgrondin@qualisocial.com>
2024-10-21 11:40:24 +0200

    edit mailer

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -10,6 +10,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Send email
