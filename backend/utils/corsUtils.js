665b0712345d0773fdc0fab822dd54103db1e3c3
Florent Grondin <fgrondin@qualisocial.com>
2024-10-23 23:27:10 +0200

    test cors

--- backend/utils/corsUtils.js
+++ backend/utils/corsUtils.js
@@ -10,7 +10,7 @@ const corsOptions = {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error(`${origin} Not allowed by CORS`));
         }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
