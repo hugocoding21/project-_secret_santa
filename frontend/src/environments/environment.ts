7c0ac983edbeea32d5520d872ebf284a9d175e8d
Florent Grondin <fgrondin@qualisocial.com>
2024-10-24 12:14:32 +0200

    modify env and and cors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,11 @@
 import { Environment } from './environment.model';
 
+//add url for prod when
 export const environment: Environment = {
-  production: false,
-  baseApiUrl: 'http://localhost:3001',
-  baseUrl: 'http://localhost:4200',
+  production: true,
+  baseApiUrl: 'https://secret-santa-project.onrender.com',
+  baseUrl: 'https://frontend-seven-sigma-69.vercel.app',
   security: {
-    allowedOrigins: 'http://localhost:4200',
+    allowedOrigins: 'https://frontend-seven-sigma-69.vercel.app', 
   },
 };
